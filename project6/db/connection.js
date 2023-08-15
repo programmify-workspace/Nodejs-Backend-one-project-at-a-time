738d91893fedf1cf6f83fd5939e23e68c11aa491
rachael-codes <120269685+rachael-codes@users.noreply.github.com>
2023-08-15 16:18:30 +0100

    Initial commit

---
+++ project6/db/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database
+});
+
+module.exports = pool;
